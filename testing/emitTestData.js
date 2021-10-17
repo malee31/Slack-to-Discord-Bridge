e934436643983e9094fc771f933ff59f60dc222f
Marvin Lee <43216761+malee31@users.noreply.github.com>
2021-10-17 13:35:52 -0700

    Neatened up emit script

--- testing/emitTestData.js
+++ testing/emitTestData.js
@@ -17,20 +17,98 @@ Promise.all([
 		});
 	}),
 ]).then(() => {
-	const emitWhich = process.argv.slice(2);
-	const enumerateFirstArg = Number(emitWhich[0]);
-	let selectedCase;
-
-	if(!isNaN(enumerateFirstArg)) {
-		if(enumerateFirstArg >= 10) throw "Index too large for latest events. Use 0-9 or specify an event type";
-		selectedCase = testData.latest[enumerateFirstArg];
-	} else {
-		selectedCase = testData[emitWhich[0]][Number(emitWhich[1])];
-	}
-	console.log(`[${emitWhich.join(", ")}] EMITTING SUBTYPE: [${selectedCase.thread_ts ? "THREAD" : "NORMAL"}] ${selectedCase.subtype || "message"}`);
 	SlackManager.events.on("message", DiscordManager.handleMessages);
 	SlackManager.events.on("change", DiscordManager.handleChanges);
 	SlackManager.events.on("delete", DiscordManager.handleDeletes);
 	SlackManager.events.on("channel_update", DiscordManager.handleChannelUpdates);
-	SlackManager.SlackHTTPServerEventAdapter.emit("message", selectedCase);
+}).then(async () => {
+	const args = process.argv.slice(2);
+	let selectedCases = [];
+	switch(selectMode(args)) {
+		case "latest":
+			selectedCases.push(latestCase(args[0]));
+			break;
+		case "pair":
+			addPairs(selectedCases, args);
+			break;
+		case "prompt":
+			// TODO
+			break;
+		default:
+			throw "If you see this, emitTestData.js is broken.";
+	}
+
+	for(const testCase of selectedCases) {
+		const messageType = testCase.thread_ts ? "THREAD" : "NORMAL";
+		const subtype = testCase.subtype || "message";
+		console.log(`EMITTING SUBTYPE: [${messageType}] ${subtype}`);
+
+		SlackManager.SlackHTTPServerEventAdapter.emit("message", testCase);
+	}
+
+	console.log("===== Emitting Complete =====");
+	process.exit(0);
 });
+
+function selectMode(args) {
+	let mode;
+	switch(args.length) {
+		case 0:
+			mode = "prompt";
+			break;
+		case 1:
+			mode = "latest";
+			break;
+		case 2:
+			mode = "pair";
+			break;
+		default:
+			if(args.length % 2 === 0) {
+				mode = "pair";
+			} else {
+				throw "Odd number of arguments provided (>2).\nPlease enter in 0-2 arguments or 2+ in pairs";
+			}
+	}
+	return mode;
+}
+
+function toIntWithCheck(numString) {
+	let caseNum = Number(numString);
+
+	if(isNaN(caseNum)) {
+		throw `Case Number is not a Number: [${numString}]`;
+	}
+
+	return Math.floor(caseNum);
+}
+
+function latestCase(caseNumArg) {
+	let caseNum = toIntWithCheck(caseNumArg);
+
+	if(caseNum >= 10) {
+		throw "Index too large (0-9 Only). Latest events only store up to 10 items.\nInput Provided: ${caseNum}";
+	}
+
+	return testData.latest[Math.floor(caseNum)];
+}
+
+function addPairs(selectedCases, pairedArgs) {
+	for(let pairIndex = 0; pairIndex < pairedArgs.length; pairIndex += 2) {
+		let caseType = pairedArgs[pairIndex];
+		let caseNum = toIntWithCheck(pairedArgs[pairIndex + 1]);
+		if(!testData[caseType]) {
+			throw `Invalid Case Type: [${caseType}]`;
+		}
+
+		const cases = testData[caseType];
+		if(cases.length === 0) {
+			throw `There are no cases saved in case type [${caseType}].\nRun collectTestData.js and intercept one first.`;
+		}
+
+		if(cases.length <= caseNum) {
+			throw `Invalid Case Index [${caseNum}]. Case type [${caseType}] only has indices 0-${cases.length - 1}`;
+		}
+
+		selectedCases.push(cases[caseNum]);
+	}
+}
